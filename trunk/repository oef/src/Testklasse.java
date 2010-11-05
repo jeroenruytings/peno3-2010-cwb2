b2300086f083270bff8af99e4e7e80d0a24d189f
shanatjee_x@hotmail.com <shanatjee_x@hotmail.com@d79942cf-46e5-cbf0-422a-7ec671679747>
2010-11-05 14:40:39 +0000

    mkjdf

--- trunk/repository oef/src/Testklasse.java
+++ trunk/repository oef/src/Testklasse.java
@@ -3,6 +3,11 @@ public class Testklasse {
 	
 	public Testklasse(){}
 		
+		public void PrintenBovenaan(){
+			System.out.println("Hoi An");
+		}
+		
+		
 		public void Printen()
 		{
 			System.out.println("Hello World");
