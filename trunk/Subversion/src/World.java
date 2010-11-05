9f9f78fc0d2a96d42fd5767354a110526949c51d
stijn.meul <stijn.meul@d79942cf-46e5-cbf0-422a-7ec671679747>
2010-11-05 14:01:02 +0000

    Extra println

--- trunk/Subversion/src/World.java
+++ trunk/Subversion/src/World.java
@@ -5,6 +5,7 @@ public static void main(String[] args) {
 	System.out.println("Hello!");
 	System.out.println("Hello World!");
 	System.out.println("en Leuven ook!");
+	System.out.println("de groeten van alle PENO dudes!");
 }
 
 }
