35f1ffd90ed302d512793ba022e8263c921d24aa
jeroenruytings <jeroenruytings@d79942cf-46e5-cbf0-422a-7ec671679747>
2010-11-05 14:10:59 +0000

    Mopke resolved

--- trunk/Subversion/src/World.java
+++ trunk/Subversion/src/World.java
@@ -5,7 +5,7 @@ public static void main(String[] args) {
 	System.out.println("Hello!");
 	System.out.println("Hello World!");
 	System.out.println("en Leuven ook!");
-	System.out.println("de groeten van alle PENO dudes!"); //LOL
+	System.out.println("de groeten van alle PENO dudes!");
 }
 
 }
