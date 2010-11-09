f029bd3a54a7d19cb1d352b4a898d32ed1173677
jeroenruytings <jeroenruytings@d79942cf-46e5-cbf0-422a-7ec671679747>
2010-11-09 15:44:37 +0000

    Announcement aangemaakt

--- Mobile Toledo/src/cw/kuleuven/be/peno1011/cwb2/Announcement.java
+++ Mobile Toledo/src/cw/kuleuven/be/peno1011/cwb2/Announcement.java
@@ -1,20 +1,46 @@
 package cw.kuleuven.be.peno1011.cwb2;
 
 import java.util.Calendar;
+import java.util.Date;
 
 public class Announcement {
 	
-	private Calendar date;
+	private final Calendar calendar = Calendar.getInstance();
+	private final Date date;
+	private final User user;
+	private String title;
+	private final Course course;
 	
-	public Announcement() {
-		
-		
-		
-		Message	String
-		Date	Date
-		UserID (User)	String
-		Title	String
-		CourseCode (Course)	String
-
+	public Announcement(User user, Course course, String title) {
+		 date = calendar.getTime();
+		 this.user = user;
+		 this.course = course;
+		 setTitle(title);
 	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Calendar getCalendar() {
+		return calendar;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public Course getCourse() {
+		return course;
+	}
+	
+	
 }
