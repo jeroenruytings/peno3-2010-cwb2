f5bf45d2670acb8222488a183b1615c2b24822f1
jeroenruytings <jeroenruytings@d79942cf-46e5-cbf0-422a-7ec671679747>
2010-11-09 16:21:56 +0000

    Question

---
+++ trunk/Mobile Toledo/src/cw/kuleuven/be/peno1011/cwb2/Question.java
@@ -0,0 +1,29 @@
+package cw.kuleuven.be.peno1011.cwb2;
+
+public class Question {
+	
+	private User questioner;
+	private Lecture lecture;
+	
+	public Question(User questioner, Lecture lecture) {
+		setQuestioner(questioner);
+		setLecture(lecture);
+	}
+	
+	public User getQuestioner() {
+		return questioner;
+	}
+
+	public void setQuestioner(User questioner) {
+		this.questioner = questioner;
+	}
+
+	public Lecture getLecture() {
+		return lecture;
+	}
+
+	public void setLecture(Lecture lecture) {
+		this.lecture = lecture;
+	}
+
+}
