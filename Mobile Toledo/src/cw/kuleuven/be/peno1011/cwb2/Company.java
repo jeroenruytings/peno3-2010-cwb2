4f2a695c858ed0633ef3825704427380684f7ef7
fredy2479@hotmail.com <fredy2479@hotmail.com@d79942cf-46e5-cbf0-422a-7ec671679747>
2010-11-09 15:46:46 +0000

    met nieuwe constructor van building

--- Mobile Toledo/src/cw/kuleuven/be/peno1011/cwb2/Company.java
+++ Mobile Toledo/src/cw/kuleuven/be/peno1011/cwb2/Company.java
@@ -2,19 +2,25 @@ package cw.kuleuven.be.peno1011.cwb2;
 
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Map;
 
 public class Company extends Building
 {
 	private Catalogue catalogue;
 	
-	public Company(Catalogue catalogue, String name, HashMap<String, String> openinghours,String phonenumber, URL groundplan, boolean isRentable, ArrayList<Room> rooms, Location location)
+
 	
+	public Company(Catalogue catalogue, int locationNr, int[] coordinates, String street,
+			int number, int zipCode, String city, String name,
+			Map<String, String> openinghours, String phoneNumber, URL map,
+			boolean isRentable, ArrayList<Room> rooms, Location location)
 	{
-		super(name, openinghours,phonenumber,groundplan, isRentable, rooms, location);
+		super(locationNr, coordinates, street, number, zipCode, city, name,
+				openinghours, phoneNumber, map, isRentable, rooms, location);
 		this.catalogue = catalogue;
-	}
 	
+	}
+
 	public Catalogue getCatalogue()
 	{
 		return catalogue;
