03df3c330b2228bfdfa203f80e45c0d144221963
stijn.meul <stijn.meul@d79942cf-46e5-cbf0-422a-7ec671679747>
2010-11-09 16:36:25 +0000

    Agenda aangemaakt (op oud-klassendiagram heet dit Calendar)

---
+++ Mobile Toledo/src/cw/kuleuven/be/peno1011/cwb2/Agenda.java
@@ -0,0 +1,20 @@
+package cw.kuleuven.be.peno1011.cwb2;
+
+import java.util.ArrayList;
+
+public class Agenda {
+	
+	private ArrayList<Event> events = new ArrayList<Event>();
+	
+	public Agenda(){
+		
+	}
+
+	public void setEvents(ArrayList<Event> events) {
+		this.events = events;
+	}
+
+	public ArrayList<Event> getEvents() {
+		return events;
+	}
+}
