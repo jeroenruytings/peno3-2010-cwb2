281918f88e23e68c48a7a7de00b5de6dd72e629a
jeroenruytings <jeroenruytings@d79942cf-46e5-cbf0-422a-7ec671679747>
2010-11-09 15:54:12 +0000

    ISP aangepast

--- Mobile Toledo/src/cw/kuleuven/be/peno1011/cwb2/ISP.java
+++ Mobile Toledo/src/cw/kuleuven/be/peno1011/cwb2/ISP.java
@@ -9,9 +9,41 @@ public class ISP {
 	private int phase;
 	
 	public ISP(ArrayList<Course> courses, String group, String studies,int phase) {
+		setCourses(courses);
+		setGroup(group);
+		setStudies(studies);
+		setPhase(phase);
+	}
+
+	public ArrayList<Course> getCourses() {
+		return courses;
+	}
+
+	public void setCourses(ArrayList<Course> courses) {
 		this.courses = courses;
+	}
+
+	public String getGroup() {
+		return group;
+	}
+
+	public void setGroup(String group) {
 		this.group = group;
+	}
+
+	public String getStudies() {
+		return studies;
+	}
+
+	public void setStudies(String studies) {
 		this.studies = studies;
+	}
+
+	public int getPhase() {
+		return phase;
+	}
+
+	public void setPhase(int phase) {
 		this.phase = phase;
 	}
 	
