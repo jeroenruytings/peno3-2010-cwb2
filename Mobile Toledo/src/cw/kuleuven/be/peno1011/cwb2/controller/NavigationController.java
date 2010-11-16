1a43c0d6af4108b934ce1c7d559ec18c457345e3
stijn.meul <stijn.meul@d79942cf-46e5-cbf0-422a-7ec671679747>
2010-11-16 16:02:24 +0000

    Added a drawRoutemethod

--- Mobile Toledo/src/cw/kuleuven/be/peno1011/cwb2/controller/NavigationController.java
+++ Mobile Toledo/src/cw/kuleuven/be/peno1011/cwb2/controller/NavigationController.java
@@ -1,5 +1,7 @@
 package cw.kuleuven.be.peno1011.cwb2.controller;
 
+import cw.kuleuven.be.peno1011.cwb2.model.Location;
+
 public class NavigationController {
 	
 	private static NavigationController navigationController;
@@ -15,6 +17,11 @@ public class NavigationController {
 		return navigationController;
 	}
 	
-	
+	public void drawRoute(Location location) {
+		int[] locCoordinates = location.getCoordinates();
+		int X=locCoordinates[1];
+		int Y=locCoordinates[2];
+		
+	}
 
 }
