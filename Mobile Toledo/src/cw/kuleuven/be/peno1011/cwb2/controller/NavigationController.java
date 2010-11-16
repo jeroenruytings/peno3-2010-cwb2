d49407c1c2470dc9d7ce5e92492a10e8e7b7d338
wimmar0 <wimmar0@d79942cf-46e5-cbf0-422a-7ec671679747>
2010-11-16 16:25:13 +0000

    locatiebepaling in controller

--- Mobile Toledo/src/cw/kuleuven/be/peno1011/cwb2/controller/NavigationController.java
+++ Mobile Toledo/src/cw/kuleuven/be/peno1011/cwb2/controller/NavigationController.java
@@ -1,10 +1,15 @@
 package cw.kuleuven.be.peno1011.cwb2.controller;
 
-import cw.kuleuven.be.peno1011.cwb2.model.Location;
+//import cw.kuleuven.be.peno1011.cwb2.model.GPSLocation;
+
+import android.location.Location;
+import android.location.LocationManager;
 
 public class NavigationController {
 	
 	private static NavigationController navigationController;
+	private Location location;
+	private LocationManager locationManager;
 	
 	private NavigationController() {
 		
@@ -16,12 +21,20 @@ public class NavigationController {
 		}
 		return navigationController;
 	}
-	
-	public void drawRoute(Location location) {
-		int[] locCoordinates = location.getCoordinates();
-		int X=locCoordinates[1];
-		int Y=locCoordinates[2];
-		
+	public void setLocation()
+	{
+		location = locationManager.getLastKnownLocation("xyz");
 	}
+	public Location getLocation()
+	{
+		return location;
+	}
+//	public void drawRoute(Location location) {
+//		int[] locCoordinates = location.getCoordinates();
+//		int X=locCoordinates[1];
+//		int Y=locCoordinates[2];
+//		
+//	}
+	
 
 }
