c33b0e446313f6d7ba13d479a41dd586ce9aee79
wimmar0 <wimmar0@d79942cf-46e5-cbf0-422a-7ec671679747>
2010-11-17 00:10:34 +0000

    MainController aangemaakt, NavigationController aangepast

---
+++ Mobile Toledo/src/cw/kuleuven/be/peno1011/cwb2/controller/MainController.java
@@ -0,0 +1,11 @@
+package cw.kuleuven.be.peno1011.cwb2.controller;
+
+import cw.kuleuven.be.peno1011.cwb2.model.User;
+
+public class MainController {
+	private static User user;
+	
+	public static User getUser(){
+		return user;
+	}
+}
