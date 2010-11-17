c53dcc3f91f1639255e7fac78abc150aed2e89b8
jeroenruytings <jeroenruytings@d79942cf-46e5-cbf0-422a-7ec671679747>
2010-11-17 20:29:02 +0000

    Startscherm en mainmenu in orde gebracht (vooral layout)

---
+++ Mobile Toledo/src/cw/kuleuven/be/peno1011/cwb2/view/MainMenu.java
@@ -0,0 +1,16 @@
+package cw.kuleuven.be.peno1011.cwb2.view;
+
+import cw.kuleuven.be.peno1011.cwb2.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainMenu extends Activity {
+	
+    protected void onCreate(Bundle savedInstanceState) {
+
+	super.onCreate(savedInstanceState);
+    
+    setContentView(R.layout.mainmenu);
+    setTitle(R.string.mainmenu);
+    }
+}
---
+++ Mobile Toledo/src/cw/kuleuven/be/peno1011/cwb2/view/MobileToledo.java
@@ -0,0 +1,73 @@
+package cw.kuleuven.be.peno1011.cwb2.view;
+
+import cw.kuleuven.be.peno1011.cwb2.R;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MobileToledo extends Activity {
+	
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        setContentView(R.layout.mobiletoledo);
+        setTitle(R.string.mobiletoledo);
+        
+        Button loginbutton = (Button) findViewById(R.id.login);
+
+        loginbutton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+            	EditText mUsername = (EditText) findViewById(R.id.musername);
+                EditText mPassword = (EditText) findViewById(R.id.mpassword);
+            	String username = mUsername.getText().toString();
+            	String password = mPassword.getText().toString();
+            	if (username.equals("") || password.equals("")){
+            		Context context = getApplicationContext();
+            		CharSequence text = "You have to enter your username" +
+            				" and your password!";
+            		int duration = Toast.LENGTH_SHORT;
+            		Toast toast = Toast.makeText(context, text, duration);
+            		toast.show();
+            	}
+            	else
+            	   login(username, password) ;
+            }
+
+        });
+    }
+
+	private void login(String username, String password) {
+		if(verifyUsername(username) && verifyPassword(password)){
+			Intent i = new Intent(this, MainMenu.class);
+	        startActivity(i);
+	        finish();
+		}
+		else{
+			Context context = getApplicationContext();
+    		CharSequence text = "Invalid combination username-password!";
+    		int duration = Toast.LENGTH_SHORT;
+    		Toast toast = Toast.makeText(context, text, duration);
+    		toast.show();
+		}
+	}
+
+	private boolean verifyPassword(String password) {
+		// TODO Moet nog gecontroleerd worden
+		return true;
+	}
+
+	private boolean verifyUsername(String username) {
+		// TODO Moet nog gecontroleerd worden
+		return true;
+	}
+	
+	
+	
+}
