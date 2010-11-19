7b77a0161aae4ae310ef34f49ea0ee3d04b5c3eb
wimmar0 <wimmar0@d79942cf-46e5-cbf0-422a-7ec671679747>
2010-11-19 16:35:43 +0000

    update

--- trunk/Mobile Toledo/src/cw/kuleuven/be/peno1011/cwb2/view/MakeAnnouncement.java
+++ trunk/Mobile Toledo/src/cw/kuleuven/be/peno1011/cwb2/view/MakeAnnouncement.java
@@ -1,8 +1,12 @@
 package cw.kuleuven.be.peno1011.cwb2.view;
 
 import cw.kuleuven.be.peno1011.cwb2.R;
+import cw.kuleuven.be.peno1011.cwb2.database.AnnouncementDAO;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MakeAnnouncement extends Activity{
 	protected void OnCreate(Bundle savedInstanceState){
@@ -10,6 +14,31 @@ public class MakeAnnouncement extends Activity{
 		
 		setContentView(R.layout.makeannouncement);
 	    setTitle(R.string.makeannouncement);
+	    
+	    Button resetbutton = (Button) findViewById(R.id.reset);
+
+        resetbutton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+        	    EditText mTitle = (EditText) findViewById(R.id.musername);
+                EditText mMessage = (EditText) findViewById(R.id.mpassword);
+            	mTitle.setText("");
+            	mMessage.setText("");
+            }
+
+        });
+        Button submitbutton = (Button) findViewById(R.id.submit);
+
+        submitbutton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+            	EditText mTitle = (EditText) findViewById(R.id.musername);
+                EditText mMessage = (EditText) findViewById(R.id.mpassword);
+                AnnouncementDAO dao = AnnouncementDAO.getInstance();
+               	dao.insert(mTitle.getText().toString(),mMessage.getText().toString());
+            }
+
+        });
 	}
 	
 }
