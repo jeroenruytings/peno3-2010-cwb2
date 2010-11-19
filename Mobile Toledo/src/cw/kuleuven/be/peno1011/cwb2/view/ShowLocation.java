271683ffbdd22ab01bea9d7682a78af7114be02c
shanatjee_x@hotmail.com <shanatjee_x@hotmail.com@d79942cf-46e5-cbf0-422a-7ec671679747>
2010-11-19 14:06:34 +0000

    showlocation activity

---
+++ Mobile Toledo/src/cw/kuleuven/be/peno1011/cwb2/view/ShowLocation.java
@@ -0,0 +1,16 @@
+package cw.kuleuven.be.peno1011.cwb2.view;
+
+
+
+import android.app.Activity;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class ShowLocation extends Activity{
+	
+
+}
