078c3c7e1c0e6388000f71b302fa927c804480b8
shanatjee_x@hotmail.com <shanatjee_x@hotmail.com@d79942cf-46e5-cbf0-422a-7ec671679747>
2010-11-19 16:02:13 +0000

    first version NavigationMenu, new intent in MainMenu

--- trunk/Mobile Toledo/src/cw/kuleuven/be/peno1011/cwb2/view/MainMenu.java
+++ trunk/Mobile Toledo/src/cw/kuleuven/be/peno1011/cwb2/view/MainMenu.java
@@ -2,7 +2,10 @@ package cw.kuleuven.be.peno1011.cwb2.view;
 
 import cw.kuleuven.be.peno1011.cwb2.R;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainMenu extends Activity {
 	
@@ -12,5 +15,17 @@ public class MainMenu extends Activity {
     
     setContentView(R.layout.mainmenu);
     setTitle(R.string.mainmenu);
+    
+    ImageButton navigationbutton = (ImageButton) findViewById(R.id.compassbutton);
+    navigationbutton.setOnClickListener(new View.OnClickListener() {
+		
+		@Override
+		public void onClick(View view) {
+			Intent intent = new Intent(MainMenu.this,NavigationMenu.class);
+			startActivity(intent);
+			finish();
+		}
+    });
     }
+    
 }
