9a26fabf6f5d5800b092f873d39a13df82adaef1
shanatjee_x@hotmail.com <shanatjee_x@hotmail.com@d79942cf-46e5-cbf0-422a-7ec671679747>
2010-11-19 16:02:13 +0000

    first version NavigationMenu, new intent in MainMenu

---
+++ Mobile Toledo/src/cw/kuleuven/be/peno1011/cwb2/view/NavigationMenu.java
@@ -0,0 +1,15 @@
+package cw.kuleuven.be.peno1011.cwb2.view;
+import cw.kuleuven.be.peno1011.cwb2.R;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NavigationMenu extends Activity{
+	
+	protected void OnCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.navigationmenu);
+	}
+
+}
