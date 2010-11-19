106febc646a125fde1c9dd9a1bf33f4151e81c2d
fredy2479@hotmail.com <fredy2479@hotmail.com@d79942cf-46e5-cbf0-422a-7ec671679747>
2010-11-19 14:33:57 +0000

    eerste volledige Activity

--- trunk/Mobile Toledo/src/cw/kuleuven/be/peno1011/cwb2/view/ShowLocation.java
+++ trunk/Mobile Toledo/src/cw/kuleuven/be/peno1011/cwb2/view/ShowLocation.java
@@ -2,6 +2,7 @@ package cw.kuleuven.be.peno1011.cwb2.view;
 
 
 
+import cw.kuleuven.be.peno1011.cwb2.R;
 import android.app.Activity;
 import android.content.Context;
 import android.location.Location;
@@ -12,5 +13,40 @@ import android.widget.TextView;
 
 public class ShowLocation extends Activity{
 	
+	private TextView latitudeField;
+	private TextView longitudeField;
+	
+	
+public void onCreate(Bundle savedInstanceState)
+{
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.navigationmenu);
+	latitudeField = (TextView) findViewById(R.id.unknown01);
+	longitudeField = (TextView) findViewById(R.id.unknown02);
 
 }
+
+public void showLocation(View view)
+{
+	switch (view.getId()) {
+	case R.id.showlocation:
+				LocationManager locationmanager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+				Location location = locationmanager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+		
+				if(location != null)
+				{
+					int lat = (int) (location.getLatitude());
+					int lng = (int) (location.getLongitude());
+					latitudeField.setText(String.valueOf(lat));
+					longitudeField.setText(String.valueOf(lng));
+				}
+				else
+				{
+					longitudeField.setText("Sorry, u werd niet gevonden");
+					latitudeField.setText("Waar zit gij ergens?");
+				}
+		break;
+		}
+	}
+}
+
