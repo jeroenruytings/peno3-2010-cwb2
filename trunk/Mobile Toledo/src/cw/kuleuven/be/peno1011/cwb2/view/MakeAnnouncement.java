6de95cca7fc6961db8e4a509b15e0990a9552df4
wimmar0 <wimmar0@d79942cf-46e5-cbf0-422a-7ec671679747>
2010-11-19 16:05:50 +0000

    melding plaatsen (view)

---
+++ trunk/Mobile Toledo/src/cw/kuleuven/be/peno1011/cwb2/view/MakeAnnouncement.java
@@ -0,0 +1,15 @@
+package cw.kuleuven.be.peno1011.cwb2.view;
+
+import cw.kuleuven.be.peno1011.cwb2.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MakeAnnouncement extends Activity{
+	protected void OnCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.makeannouncement);
+	    setTitle(R.string.makeannouncement);
+	}
+	
+}
