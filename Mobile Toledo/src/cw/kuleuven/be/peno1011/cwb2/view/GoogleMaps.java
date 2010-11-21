0941fee0855848e2dca3609785b86d949b95a850
shanatjee_x@hotmail.com <shanatjee_x@hotmail.com@d79942cf-46e5-cbf0-422a-7ec671679747>
2010-11-21 20:46:41 +0000

    google maps

---
+++ Mobile Toledo/src/cw/kuleuven/be/peno1011/cwb2/view/GoogleMaps.java
@@ -0,0 +1,59 @@
+package cw.kuleuven.be.peno1011.cwb2.view;
+import java.util.ArrayList;
+
+import cw.kuleuven.be.peno1011.cwb2.R;
+import cw.kuleuven.be.peno1011.cwb2.model.User;
+
+
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
+import android.os.Bundle;
+
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.ItemizedOverlay;
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapController;
+import com.google.android.maps.MapView;
+import com.google.android.maps.OverlayItem;
+import com.google.android.maps.MapView.LayoutParams;  
+import android.view.View;
+import android.widget.LinearLayout;
+import com.google.android.maps.Overlay;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Point;
+import android.graphics.drawable.Drawable;
+
+public class GoogleMaps extends MapActivity{
+	private MapView mapView;
+	private MapController mc;
+    private GeoPoint p;
+    private LocationManager locationManager;
+	private LocationListener listener;
+	private Location location;
+	private GeoPoint gp;
+	private String locationprovider;
+	
+	
+	@Override
+	protected boolean isRouteDisplayed() {
+	    return false;
+	}
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.googlemaps);
+	    mapView = (MapView) findViewById(R.id.mapview);
+	    mapView.setBuiltInZoomControls(true);
+        
+ 
+        
+        
+	}
+
+}
