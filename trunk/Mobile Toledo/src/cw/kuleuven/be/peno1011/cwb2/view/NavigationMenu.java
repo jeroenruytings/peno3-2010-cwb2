49d71f40ae943efd3295eb859de58a72b30dfd12
shanatjee_x@hotmail.com <shanatjee_x@hotmail.com@d79942cf-46e5-cbf0-422a-7ec671679747>
2010-11-21 20:46:41 +0000

    google maps

--- trunk/Mobile Toledo/src/cw/kuleuven/be/peno1011/cwb2/view/NavigationMenu.java
+++ trunk/Mobile Toledo/src/cw/kuleuven/be/peno1011/cwb2/view/NavigationMenu.java
@@ -1,18 +1,40 @@
 package cw.kuleuven.be.peno1011.cwb2.view;
+import com.google.android.maps.MapActivity;
+
 import cw.kuleuven.be.peno1011.cwb2.R;
 
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
-public class NavigationMenu extends Activity{
+public class NavigationMenu extends MapActivity{
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.navigationmenu);
 		setTitle(R.string.hello);
+		
+		ImageButton button1 = (ImageButton) findViewById(R.id.ownlocationbutton);
+		button1.setOnClickListener(new View.OnClickListener() {
 			
+			@Override
+			public void onClick(View arg0) {
+				Intent intent = new Intent(NavigationMenu.this,GoogleMaps.class);
+				startActivity(intent);
+
+			}
+		});
+		
 	}
 
+	@Override
+	protected boolean isRouteDisplayed() {
+		// TODO Auto-generated method stub
+		return false;
+	}
 }
