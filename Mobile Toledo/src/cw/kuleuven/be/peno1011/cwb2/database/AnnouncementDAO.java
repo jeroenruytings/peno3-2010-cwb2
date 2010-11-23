f9e804a9dc427a02b8d8aa9abac811b9ed88a718
stijn.meul <stijn.meul@d79942cf-46e5-cbf0-422a-7ec671679747>
2010-11-23 17:05:46 +0000

    AnnouncementDAO voor client-side aangepast

--- Mobile Toledo/src/cw/kuleuven/be/peno1011/cwb2/database/AnnouncementDAO.java
+++ Mobile Toledo/src/cw/kuleuven/be/peno1011/cwb2/database/AnnouncementDAO.java
@@ -5,6 +5,21 @@ import cw.kuleuven.be.peno1011.cwb2.model.Announcement;
 import cw.kuleuven.be.peno1011.cwb2.model.Course;
 import cw.kuleuven.be.peno1011.cwb2.model.User;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Date;
+
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpException;
+import org.apache.commons.httpclient.methods.PostMethod;
+import org.apache.commons.io.IOUtils;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonParseException;
+
 public class AnnouncementDAO {
 	
 	private static AnnouncementDAO announcementDAO;
@@ -20,16 +35,71 @@ public class AnnouncementDAO {
 		}
 		return announcementDAO;
 	}
-	//TO DO: opslaan i/database
-	public boolean insert(String title, String message, Course course) {
-		User prof = MainController.getUser();
-		Announcement newAnnouncement = new Announcement(prof,course,title,message);
-		boolean inserted=false;
-		return inserted;
+	
+	public void insert(String jsonannouncement) {
+		try {
+			HttpClient client = new HttpClient();
+			// maak nieuwe postmethode waarbij de posts op het domein meegegeven als parameter worden geplaatst
+			PostMethod method = new PostMethod("http://localhost:9876/AnnouncementHandler/addAnnouncement");
+			method.addParameter("announcement", jsonannouncement);
+			// voert de methode ook werkelijk uit en retourneert een onbekend getal
+			int returnCode = client.executeMethod(method);
+			// displayt de respone op de postmethode
+			System.out.println(method.getResponseBodyAsString());
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (HttpException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-	//TO DO
+	
 	public boolean delete(Announcement announcement) {
 		boolean deleted=false;
 		return deleted;
 	}
+	
+	/**
+	 * Methode moet nog uitgewerkt worden:
+	 */
+	public static void testListUsers() {
+
+		try {
+			String json = stringOfUrl("http://localhost:9876/UserHandler/listUsers");
+
+			User[] obj2 = new Gson().fromJson(json.toString(), User[].class);  
+			for (User user : obj2) {
+				System.out.println(user);	
+			}
+			
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JsonParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+	
+    public static String stringOfUrl(String addr) throws IOException {
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        URL url = new URL(addr);
+        IOUtils.copy(url.openStream(), output);
+        return output.toString();
+    }
+    
+    public static String streamToString(InputStream stream) throws IOException {
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        IOUtils.copy(stream, output);
+        return output.toString();
+    }
+    
 }
