e59918b95444e521e2b3fe0fef767b24f9f98c37
Jochentombal@gmail.com <Jochentombal@gmail.com@d79942cf-46e5-cbf0-422a-7ec671679747>
2010-11-23 16:23:01 +0000

    Nu de juiste foutloze versie

--- Mobile Toledo/src/cw/kuleuven/be/peno1011/cwb2/database/QuestionDAO.java
+++ Mobile Toledo/src/cw/kuleuven/be/peno1011/cwb2/database/QuestionDAO.java
@@ -1,36 +1,38 @@
-//	package cw.kuleuven.be.peno1011.cwb2.database;
-//
-//	import cw.kuleuven.be.peno1011.cwb2.controller.MainController;
-//	import cw.kuleuven.be.peno1011.cwb2.model.Question;
-//	import cw.kuleuven.be.peno1011.cwb2.model.Course;
-//	import cw.kuleuven.be.peno1011.cwb2.model.User;
-//
-//	public class QuestionDAO {
-//		
-//		private static QuestionDAO QuestionDAO;
-//		
-//		// Singleton
-//		private QuestionDAO(){
-//			
-//		}
-//		
-//		public static QuestionDAO getInstance() {
-//			if (QuestionDAO == null){
-//				QuestionDAO = new QuestionDAO();
-//			}
-//			return QuestionDAO;
-//		}
-//		//TO DO: opslaan i/database
-//		public boolean insert(String message, Lecture lecture) {
-//			User questioner  = MainController.getUser();
-//			Question newAnnouncement = new Announcement(prof,course,title,message);
-//			boolean inserted=false;
-//			return inserted;
-//		}
-//		//TO DO
-//		public boolean delete(Question question) {
-//			boolean deleted=false;
-//			return deleted;
-//		}
-//	}
+	package cw.kuleuven.be.peno1011.cwb2.database;
+
+	import cw.kuleuven.be.peno1011.cwb2.controller.MainController;
+	import cw.kuleuven.be.peno1011.cwb2.model.Question;
+	import cw.kuleuven.be.peno1011.cwb2.model.User;
+	import cw.kuleuven.be.peno1011.cwb2.model.Lecture;
+
+	public class QuestionDAO {
+		
+		private static QuestionDAO QuestionDAO;
+		
+		// Singleton
+		private QuestionDAO(){
+			
+		}
+		
+		public static QuestionDAO getInstance() {
+			if (QuestionDAO == null){
+				QuestionDAO = new QuestionDAO();
+			}
+			return QuestionDAO;
+		}
+		
+		//TO DO: opslaan i/database
+		public boolean insert(String message, Lecture lecture) {
+			User questioner  = MainController.getUser();
+			Question newQuestion = new Question(questioner, lecture, message);
+			boolean inserted=false;
+			return inserted;
+		}
+		//TO DO
+		public boolean delete(Question question) {
+			boolean deleted=false;
+			return deleted;
+		}
+	}
+
 
