126ba28a83298d73425829e0eef2250ec9246ffa
jochentombal@gmail.com <jochentombal@gmail.com@d79942cf-46e5-cbf0-422a-7ec671679747>
2010-11-26 16:23:31 +0000

    Er zit hier nog een fout in maar we comitten het om te checken of die op de andere pc wel compileert

--- Mobile Toledo/src/cw/kuleuven/be/peno1011/cwb2/view/Questions.java
+++ Mobile Toledo/src/cw/kuleuven/be/peno1011/cwb2/view/Questions.java
@@ -1,15 +1,100 @@
 package cw.kuleuven.be.peno1011.cwb2.view;
 
+import android.R;
 import android.app.Activity;
 import android.os.Bundle;
-import cw.kuleuven.be.peno1011.cwb2.controller.MainController;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import cw.kuleuven.be.peno1011.cwb2.database.AnnouncementDAO;
+import cw.kuleuven.be.peno1011.cwb2.model.Lecture;
+import cw.kuleuven.be.peno1011.cwb2.view.SelectCourse;
+
+
+
+
 
 public class Questions extends Activity{	
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
-		if(MainController.getUser().getLevel()==1){ //implementatie 'vragen stellen'
+		
+		setContentView(R.layout.question);
+		setContentTitle(R.string.question);
+		
+		
+		 Bundle bundle = getIntent().getExtras();
+		 final Lecture lecture = (Lecture) bundle.get("lecture"); 
+		 
+				 
+		 
+		 
+		 
+		 Button resetbutton = (Button) findViewById(R.id.reset);
+
+	        resetbutton.setOnClickListener(new View.OnClickListener() {
+
+	       public void onClick(View view) {
+	        	    EditText mTitle = (EditText) findViewById(R.id.musername);
+	                EditText mMessage = (EditText) findViewById(R.id.mpassword);
+	            	mTitle.setText("");
+	            	mMessage.setText("");
+	            }
+
+	        });
+	        
+	        
+	        Button submitbutton = (Button) findViewById(R.id.submit);
+
+	        submitbutton.setOnClickListener(new View.OnClickListener() {
+	        	
+
+	            public void onClick(View view) {
+	            	EditText mTitle = (EditText) findViewById(R.id.musername);
+	                EditText mMessage = (EditText) findViewById(R.id.mpassword);
+	                
+//	               	dao.insert(mTitle.getText().toString(),mMessage.getText().toString(),.....));
+	            }
+
+	        });
+		
+	    	
+	}
+}		
+	
+
+		/*final List<Course> courses = MainController.getUser().getIsp().getCourses();
+	    List<String> courseTitles = new ArrayList<String>();
+	    
+	    for(int i=0;i<courses.size();i++){
+	    	courseTitles.set(i, courses.get(i).getCourseName());
+	    }
+	    
+	    final Spinner s = (Spinner) findViewById(R.id.courseSpinner);
+	    ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+	            this, android.R.layout.simple_spinner_item, courseTitles);
+	    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+	    s.setAdapter(adapter);
+		
+		
+	    
+	    Button resetbutton = (Button) findViewById(R.id.reset);
+
+        resetbutton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+        	    EditText mTitle = (EditText) findViewById(R.id.musername);
+                EditText mMessage = (EditText) findViewById(R.id.mpassword);
+            	mTitle.setText("");
+            	mMessage.setText("");
+            }
+
+        });*/
+		
+        
+        
+/*		if(MainController.getUser().getLevel()==1){ //implementatie 'vragen stellen'
 //			setContentView(R.layout.posequestions);
 //		    setTitle(R.string.posequesitons);
 			//TODO/ingeven van vragen, cfr MakeAnnouncement
@@ -19,6 +104,4 @@ public class Questions extends Activity{
 //		    setTitle(R.string.showquestions);
 			//TODO/listview van alle vragen, cfr ShowAnnouncements
 		}
-	}
-
-}
+	*/
