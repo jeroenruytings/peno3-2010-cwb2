407182952b0d92208311f7352fd709a0045515c5
wimmar0 <wimmar0@d79942cf-46e5-cbf0-422a-7ec671679747>
2010-11-26 17:02:39 +0000

    meerkeuzevraag stellen view

--- Mobile Toledo/src/cw/kuleuven/be/peno1011/cwb2/view/MultipleView.java
+++ Mobile Toledo/src/cw/kuleuven/be/peno1011/cwb2/view/MultipleView.java
@@ -4,6 +4,9 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 import cw.kuleuven.be.peno1011.cwb2.R;
@@ -27,6 +30,52 @@ public class MultipleView extends Activity{
 			setContentView(R.layout.posemultiplechoice);
 		    setTitle(R.string.posemultiplechoice);
 			
+		    EditText mansw1 = (EditText) findViewById(R.id.mansw1);
+		    EditText mansw2 = (EditText) findViewById(R.id.mansw2);
+		    EditText mansw3 = (EditText) findViewById(R.id.mansw3);
+		    EditText mansw4 = (EditText) findViewById(R.id.mansw4);
+		    EditText mansw5 = (EditText) findViewById(R.id.mansw5);
+		    
+		    mansw1.setOnKeyListener(new View.OnKeyListener() {
+				@Override
+				public boolean onKey(View v, int keyCode, KeyEvent event) {
+				    TextView answ2 = (TextView) findViewById(R.id.answ2);
+				    EditText mansw2 = (EditText) findViewById(R.id.mansw2);
+					answ2.setVisibility(View.VISIBLE);
+					mansw2.setVisibility(View.VISIBLE);
+					return false;
+				}
+			});
+		    mansw2.setOnKeyListener(new View.OnKeyListener() {
+				@Override
+				public boolean onKey(View v, int keyCode, KeyEvent event) {
+				    TextView answ3 = (TextView) findViewById(R.id.answ3);
+				    EditText mansw3 = (EditText) findViewById(R.id.mansw3);
+					answ3.setVisibility(View.VISIBLE);
+					mansw3.setVisibility(View.VISIBLE);
+					return false;
+				}
+			});
+		    mansw3.setOnKeyListener(new View.OnKeyListener() {
+				@Override
+				public boolean onKey(View v, int keyCode, KeyEvent event) {
+				    TextView answ4 = (TextView) findViewById(R.id.answ4);
+				    EditText mansw4 = (EditText) findViewById(R.id.mansw4);
+					answ4.setVisibility(View.VISIBLE);
+					mansw4.setVisibility(View.VISIBLE);
+					return false;
+				}
+			});
+		    mansw4.setOnKeyListener(new View.OnKeyListener() {
+				@Override
+				public boolean onKey(View v, int keyCode, KeyEvent event) {
+				    TextView answ5 = (TextView) findViewById(R.id.answ5);
+				    EditText mansw5 = (EditText) findViewById(R.id.mansw5);
+					answ5.setVisibility(View.VISIBLE);
+					mansw5.setVisibility(View.VISIBLE);
+					return false;
+				}
+			});
 		    
 		}
 		else{ //implementatie stellen
