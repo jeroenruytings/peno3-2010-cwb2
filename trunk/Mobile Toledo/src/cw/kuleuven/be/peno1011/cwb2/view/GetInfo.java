401d79c6a91f4b4b2f7d56f1ae30dd310c66ccc9
fredy2479@hotmail.com <fredy2479@hotmail.com@d79942cf-46e5-cbf0-422a-7ec671679747>
2010-11-27 14:27:56 +0000

    eerst commit getinfo activity

---
+++ trunk/Mobile Toledo/src/cw/kuleuven/be/peno1011/cwb2/view/GetInfo.java
@@ -0,0 +1,16 @@
+package cw.kuleuven.be.peno1011.cwb2.view;
+
+import cw.kuleuven.be.peno1011.cwb2.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GetInfo extends Activity {
+	 
+	@Override
+	 protected void onCreate(Bundle savedInstanceState) {
+	     super.onCreate(savedInstanceState);
+	     setContentView(R.layout.getinfo);
+
+	}
+}
+
