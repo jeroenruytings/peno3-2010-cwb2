e4c3fd6bba0befa2d24ad0501590b167e4bbc004
fredy2479@hotmail.com <fredy2479@hotmail.com@d79942cf-46e5-cbf0-422a-7ec671679747>
2010-11-27 14:51:12 +0000

    met veranderlijke imageview

--- Mobile Toledo/src/cw/kuleuven/be/peno1011/cwb2/view/GetInfo.java
+++ Mobile Toledo/src/cw/kuleuven/be/peno1011/cwb2/view/GetInfo.java
@@ -3,6 +3,7 @@ package cw.kuleuven.be.peno1011.cwb2.view;
 import cw.kuleuven.be.peno1011.cwb2.R;
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class GetInfo extends Activity {
 	 
@@ -10,6 +11,9 @@ public class GetInfo extends Activity {
 	 protected void onCreate(Bundle savedInstanceState) {
 	     super.onCreate(savedInstanceState);
 	     setContentView(R.layout.getinfo);
+	     
+	     ImageView image = (ImageView) findViewById(R.id.locationmap);
+	     image.setImageResource(R.drawable.toledo_logo);
 
 	}
 }
