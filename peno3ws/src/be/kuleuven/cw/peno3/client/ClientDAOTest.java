c804ddc2e8608ef445f84c909278b3130a717c70
jeroenruytings@gmail.com <jeroenruytings@gmail.com@d79942cf-46e5-cbf0-422a-7ec671679747>
2010-11-29 12:49:55 +0000

    Alles werkt!

--- peno3ws/src/be/kuleuven/cw/peno3/client/ClientDAOTest.java
+++ peno3ws/src/be/kuleuven/cw/peno3/client/ClientDAOTest.java
@@ -7,21 +7,21 @@ import java.net.URL;
 
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpException;
+import org.apache.commons.httpclient.methods.GetMethod;
 import org.apache.commons.httpclient.methods.PostMethod;
 import org.apache.commons.io.IOUtils;
 import org.apache.log4j.BasicConfigurator;
+//import org.apache.log4j.BasicConfigurator;
 
 import be.kuleuven.cw.peno3.model.Announcement;
-import be.kuleuven.cw.peno3.model.Credential;
-import be.kuleuven.cw.peno3.model.User;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonParseException;
 
 public class ClientDAOTest {
 	public static void main(String[] args) {
-		BasicConfigurator.configure();
-		//testListAnnouncements();
+		//BasicConfigurator.configure();
+		testListAnnouncements();
 		testAddAnnouncements();
 	}
 
@@ -30,14 +30,25 @@ public class ClientDAOTest {
 			
 			HttpClient client = new HttpClient();
 			
-			PostMethod method = new PostMethod("http://" + ipAdress.getIp() + "/AnnouncementHandler/addAnnouncement");
-			method.addParameter("message", "Hoera! Hoera! Eerste databaserecord toegevoegd!");
-			method.addParameter("userId", "s0215121");
-//			method.addParameter("date", null);
-			method.addParameter("title", "Joepie!");
-			method.addParameter("courseCode", "1");
+			GetMethod method = new GetMethod("http://" + ipAdress.getIp() + "/AnnouncementHandler/addAnnouncement");
+			
+			String message = "Test%20test";
+			String userId = "s0199104";
+			String title = "Test";
+			String courseCode = "123";
+			String qry = "?message="+message+"&userId="+userId+"&title="+title+"&courseCode="+courseCode;
+			System.out.println(qry);
+			//method.setQueryString("?message=Hopelijk werkt dit&userId=s0199104&title=test&courseCode=123");
+			method.setQueryString(qry);
+			
+			//PostMethod method = new PostMethod("http://" + ipAdress.getIp() + "/AnnouncementHandler/addAnnouncement");
+//			method.addParameter("message", "Hoera! Hoera! Eerste databaserecord toegevoegd!");
+//			method.addParameter("userId", "s0215121");
+////			method.addParameter("date", null);
+//			method.addParameter("title", "Joepie!");
+//			method.addParameter("courseCode", "1");
 			int returnCode = client.executeMethod(method);
-
+			System.out.println(returnCode);
 			System.out.println(method.getResponseBodyAsString());
 		} catch (IllegalArgumentException e) {
 			// TODO Auto-generated catch block
@@ -54,10 +65,15 @@ public class ClientDAOTest {
 
 		try {
 			String json = stringOfUrl("http://" + ipAdress.getIp() + "/AnnouncementHandler/listAnnouncements");
-
+			System.out.println(json);
 			Announcement[] obj2 = new Gson().fromJson(json.toString(), Announcement[].class);  
 			for (Announcement announcement : obj2) {
-				System.out.println(announcement);	
+				System.out.println("Message = "+announcement.getMessage());	
+				System.out.println("Title = "+announcement.getTitle());	
+				System.out.println("UserId = "+ announcement.getUserId());	
+				System.out.println("Coursecode = "+announcement.getCourseCode());	
+				System.out.println("AnnouncementId = " + announcement.getAnnouncementId());	
+			
 			}
 			
 		} catch (MalformedURLException e) {
