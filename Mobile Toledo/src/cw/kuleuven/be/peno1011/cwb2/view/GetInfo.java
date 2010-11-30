2fd71f88e26ea8af39613e71db79ebb6e0c096c3
fredy2479@hotmail.com <fredy2479@hotmail.com@d79942cf-46e5-cbf0-422a-7ec671679747>
2010-11-30 15:46:50 +0000

    met setUri

--- Mobile Toledo/src/cw/kuleuven/be/peno1011/cwb2/view/GetInfo.java
+++ Mobile Toledo/src/cw/kuleuven/be/peno1011/cwb2/view/GetInfo.java
@@ -1,20 +1,33 @@
 package cw.kuleuven.be.peno1011.cwb2.view;
 
 import cw.kuleuven.be.peno1011.cwb2.R;
+import cw.kuleuven.be.peno1011.cwb2.controller.NavigationController;
 import android.app.Activity;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class GetInfo extends Activity {
 	 
+	private NavigationController control;
+
 	@Override
 	 protected void onCreate(Bundle savedInstanceState) {
-	     super.onCreate(savedInstanceState);
-	     setContentView(R.layout.getinfo);
-	     
+		
+		 super.onCreate(savedInstanceState);
+		 setContentView(R.layout.getinfo);
+		 
+		 Bundle b = this.getIntent().getExtras();
+		 String location = b.getString("autocomplete_building");
+		 TextView locationname = (TextView) findViewById(R.id.locationname);
+	  	 locationname.setText(location);
+		 
+	     control = NavigationController.getInstance();
 	     ImageView image = (ImageView) findViewById(R.id.locationmap);
-	     image.setImageResource(R.drawable.toledo_logo);
-
+	     image.setImageURI(control.getGoogleMap(location));
+	     
+	    
 	}
 }
 
