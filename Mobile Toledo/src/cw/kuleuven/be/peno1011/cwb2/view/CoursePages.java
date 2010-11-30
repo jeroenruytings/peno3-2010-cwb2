4ec23bfd34380af916e043bf191a4b3d7755c49f
wimmar0 <wimmar0@d79942cf-46e5-cbf0-422a-7ec671679747>
2010-11-30 14:11:59 +0000

    coursepages update

--- Mobile Toledo/src/cw/kuleuven/be/peno1011/cwb2/view/CoursePages.java
+++ Mobile Toledo/src/cw/kuleuven/be/peno1011/cwb2/view/CoursePages.java
@@ -8,54 +8,61 @@ import android.os.Bundle;
 import android.view.Window;
 import android.widget.TabHost;
 import android.widget.TextView;
+import android.widget.Toast;
 import cw.kuleuven.be.peno1011.cwb2.R;
 import cw.kuleuven.be.peno1011.cwb2.model.Lecture;
 
 public class CoursePages extends TabActivity{
 	public void onCreate(Bundle savedInstanceState) {
-	    super.onCreate(savedInstanceState);
-	    setContentView(R.layout.coursepages);	  
+	    super.onCreate(savedInstanceState);	  
 
 		requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);		
 		setContentView(R.layout.coursepages);
 		getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.title);	
-		Bundle bundle = getIntent().getExtras();
-	    final String courseTitle = (String) bundle.get("courseTitle");
-        ((TextView)findViewById(R.id.titlebar)).setText(courseTitle);
-        
-        
-	    Resources res = getResources(); // Resource object to get Drawables
-	    TabHost tabHost = getTabHost();  // The activity TabHost
-	    TabHost.TabSpec spec;  // Resusable TabSpec for each tab
-	    Intent intent;  // Reusable Intent for each tab
-
-
-	    // Create an Intent to launch an Activity for the tab (to be reused)
-	    intent = new Intent().setClass(this, CourseInfo.class);
-	    intent.putExtra("courseTitle", courseTitle);
-
-	    // Initialize a TabSpec for each tab and add it to the TabHost
-	    spec = tabHost.newTabSpec("Info").setIndicator("Info",
-	                      res.getDrawable(R.drawable.tab_info))
-	                  .setContent(intent);
-	    tabHost.addTab(spec);
-
-	    // Do the same for the other tabs
-	    intent = new Intent().setClass(this, ShowAnnouncements.class);
-	    intent.putExtra("courseTitle", courseTitle);
-	    //TODO/vak meegeven als extra
-	    spec = tabHost.newTabSpec("Meldingen").setIndicator("Meldingen",
-	                      res.getDrawable(R.drawable.tab_announcements))
-	                  .setContent(intent);
-	    tabHost.addTab(spec);
-
-	    intent = new Intent().setClass(this, CourseDocuments.class);
-	    intent.putExtra("courseTitle", courseTitle);
-	    spec = tabHost.newTabSpec("Documenten").setIndicator("Documenten",
-	                      res.getDrawable(R.drawable.tab_docs))
-	                  .setContent(intent);
-	    tabHost.addTab(spec);
-
-	    tabHost.setCurrentTab(2);
+		
+		try{
+			Bundle bundle = getIntent().getExtras();
+		    final String courseTitle = (String) bundle.get("courseTitle");
+	        ((TextView)findViewById(R.id.titlebar)).setText(courseTitle);
+	        
+	        
+		    Resources res = getResources(); // Resource object to get Drawables
+		    TabHost tabHost = getTabHost();  // The activity TabHost
+		    TabHost.TabSpec spec;  // Resusable TabSpec for each tab
+		    Intent intent;  // Reusable Intent for each tab
+	
+	
+		    // Create an Intent to launch an Activity for the tab (to be reused)
+		    intent = new Intent().setClass(this, CourseInfo.class);
+		    intent.putExtra("courseTitle", courseTitle);
+	
+		    // Initialize a TabSpec for each tab and add it to the TabHost
+		    spec = tabHost.newTabSpec("Info").setIndicator("Info",
+		                      res.getDrawable(R.drawable.tab_info))
+		                  .setContent(intent);
+		    tabHost.addTab(spec);
+	
+		    // Do the same for the other tabs
+		    intent = new Intent().setClass(this, ShowAnnouncements.class);
+		    intent.putExtra("courseTitle", courseTitle);
+		    //TODO/vak meegeven als extra
+		    spec = tabHost.newTabSpec("Meldingen").setIndicator("Meldingen",
+		                      res.getDrawable(R.drawable.tab_announcements))
+		                  .setContent(intent);
+		    tabHost.addTab(spec);
+	
+		    intent = new Intent().setClass(this, CourseDocuments.class);
+		    intent.putExtra("courseTitle", courseTitle);
+		    spec = tabHost.newTabSpec("Documenten").setIndicator("Documenten",
+		                      res.getDrawable(R.drawable.tab_docs))
+		                  .setContent(intent);
+		    tabHost.addTab(spec);
+	
+		    tabHost.setCurrentTab(2);
+		}
+		catch(NullPointerException ne){
+			Toast.makeText(getApplicationContext(), "Geen vak gespecifieerd.",
+			          Toast.LENGTH_LONG).show();
+		}
 	}
 }
--- Mobile Toledo/src/cw/kuleuven/be/peno1011/cwb2/view/MakeAnnouncement.java
+++ Mobile Toledo/src/cw/kuleuven/be/peno1011/cwb2/view/MakeAnnouncement.java
@@ -14,6 +14,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MakeAnnouncement extends Activity{	
 	
@@ -24,43 +25,49 @@ public class MakeAnnouncement extends Activity{
 		setContentView(R.layout.makeannouncement);
 	    setTitle(R.string.makeannouncement);
 	    
-	    final List<Course> courses = MainController.getUser().getIsp().getCourses();
-	    List<String> courseTitles = new ArrayList<String>();
-	    for(int i=0;i<courses.size();i++){
-	    	courseTitles.set(i, courses.get(i).getCourseName());
+	    if(MainController.getUser().getLevel()>1){
+		    final List<Course> courses = MainController.getUser().getIsp().getCourses();
+		    List<String> courseTitles = new ArrayList<String>();
+		    for(int i=0;i<courses.size();i++){
+		    	courseTitles.set(i, courses.get(i).getCourseName());
+		    }
+		    final Spinner s = (Spinner) findViewById(R.id.courseSpinner);
+		    ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+		            this, android.R.layout.simple_spinner_item, courseTitles);
+		    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		    s.setAdapter(adapter);
+		    
+		    Button resetbutton = (Button) findViewById(R.id.reset);
+		    //TODO/ resetbutton werkt nog niet
+	        resetbutton.setOnClickListener(new View.OnClickListener() {
+	
+	            public void onClick(View view) {
+	        	    EditText mTitle = (EditText) findViewById(R.id.musername);
+	                EditText mMessage = (EditText) findViewById(R.id.mpassword);
+	            	mTitle.setText("");
+	            	mMessage.setText("");
+	            }
+	
+	        });
+	        Button submitbutton = (Button) findViewById(R.id.submit);
+	
+	        submitbutton.setOnClickListener(new View.OnClickListener() {
+	
+	            public void onClick(View view) {
+	            	EditText mTitle = (EditText) findViewById(R.id.musername);
+	                EditText mMessage = (EditText) findViewById(R.id.mpassword);
+	                int courseLocation = s.getSelectedItemPosition();
+	                AnnouncementDAO dao = AnnouncementDAO.getInstance();
+	                //TODO/ in database steken.
+	//               	dao.insert(mTitle.getText().toString(),mMessage.getText().toString(),courses.get(courseLocation));
+	            }
+	
+	        });
 	    }
-	    final Spinner s = (Spinner) findViewById(R.id.courseSpinner);
-	    ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-	            this, android.R.layout.simple_spinner_item, courseTitles);
-	    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-	    s.setAdapter(adapter);
-	    
-	    Button resetbutton = (Button) findViewById(R.id.reset);
-	    //TODO/ resetbutton werkt nog niet
-        resetbutton.setOnClickListener(new View.OnClickListener() {
-
-            public void onClick(View view) {
-        	    EditText mTitle = (EditText) findViewById(R.id.musername);
-                EditText mMessage = (EditText) findViewById(R.id.mpassword);
-            	mTitle.setText("");
-            	mMessage.setText("");
-            }
-
-        });
-        Button submitbutton = (Button) findViewById(R.id.submit);
-
-        submitbutton.setOnClickListener(new View.OnClickListener() {
-
-            public void onClick(View view) {
-            	EditText mTitle = (EditText) findViewById(R.id.musername);
-                EditText mMessage = (EditText) findViewById(R.id.mpassword);
-                int courseLocation = s.getSelectedItemPosition();
-                AnnouncementDAO dao = AnnouncementDAO.getInstance();
-                //TODO/ in database steken.
-//               	dao.insert(mTitle.getText().toString(),mMessage.getText().toString(),courses.get(courseLocation));
-            }
-
-        });
+	    else{
+	    	Toast.makeText(getApplicationContext(), "Gebruiker mag geen announcements aanmaken. Enkel proffesoren/assistenten!",
+			          Toast.LENGTH_LONG).show();
+	    }
 
 	}
 	
