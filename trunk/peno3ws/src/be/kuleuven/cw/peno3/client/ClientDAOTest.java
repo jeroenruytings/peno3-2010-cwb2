a6bdbcc406b3aaa8596d84a20449bcf707517021
stijn.meul <stijn.meul@d79942cf-46e5-cbf0-422a-7ec671679747>
2010-11-30 14:34:47 +0000

    getAnnouncementmethodes toegevoegd clientDAOtest deze nieuwe methodes doen checken

--- trunk/peno3ws/src/be/kuleuven/cw/peno3/client/ClientDAOTest.java
+++ trunk/peno3ws/src/be/kuleuven/cw/peno3/client/ClientDAOTest.java
@@ -23,47 +23,10 @@ import com.google.gson.JsonParseException;
 
 public class ClientDAOTest {
 	public static void main(String[] args) {
-		//BasicConfigurator.configure();
-		testListAnnouncements();
+		//testListAnnouncements();
 		testAddAnnouncements();
+		testGetAnnouncement();
 	}
-
-/*	private static void testAddAnnouncements() {
-		try {
-			
-			HttpClient client = new HttpClient();
-			
-			GetMethod method = new GetMethod("http://" + ipAdress.getIp() + "/AnnouncementHandler/addAnnouncement");
-			
-			String message = "Test%20test";
-			String userId = "s0199104";
-			String title = "Test";
-			String courseCode = "123";
-			String qry = "?message="+message+"&userId="+userId+"&title="+title+"&courseCode="+courseCode;
-			System.out.println(qry);
-			//method.setQueryString("?message=Hopelijk werkt dit&userId=s0199104&title=test&courseCode=123");
-			method.setQueryString(qry);
-			
-			//PostMethod method = new PostMethod("http://" + ipAdress.getIp() + "/AnnouncementHandler/addAnnouncement");
-//			method.addParameter("message", "Hoera! Hoera! Eerste databaserecord toegevoegd!");
-//			method.addParameter("userId", "s0215121");
-////			method.addParameter("date", null);
-//			method.addParameter("title", "Joepie!");
-//			method.addParameter("courseCode", "1");
-			int returnCode = client.executeMethod(method);
-			System.out.println(returnCode);
-			System.out.println(method.getResponseBodyAsString());
-		} catch (IllegalArgumentException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (HttpException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}*/
 	
 	private static void testAddAnnouncements() {
 		try {
@@ -124,6 +87,41 @@ public class ClientDAOTest {
 			e.printStackTrace();
 		}
 
+	}
+	
+	public static void testGetAnnouncement() {
+		try {
+			HttpClient client = new HttpClient();
+			
+			PostMethod method = new PostMethod("http://" + ipAdress.getIp() + "/AnnouncementHandler/getAnnouncementByWord");
+			method.addParameter("word", "test");
+			int returnCode = client.executeMethod(method);
+			String json = method.getResponseBodyAsString();
+			if(json.contains("[]")) {
+					System.out.println("Geen zoekresultaten gevonden");
+				}
+			else {
+				System.out.println(json);
+				Announcement[] obj2 = new Gson().fromJson(json.toString(), Announcement[].class);  
+				for (Announcement announcement : obj2) {
+					
+					System.out.println("Message = "+announcement.getMessage());	
+					System.out.println("Title = "+announcement.getTitle());	
+					System.out.println("UserId = "+ announcement.getUserId());	
+					System.out.println("Coursecode = "+announcement.getCourseCode());	
+					System.out.println("AnnouncementId = " + announcement.getAnnouncementId());	
+				}
+			}
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JsonParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
     public static String stringOfUrl(String addr) throws IOException {
