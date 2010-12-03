0f891d8e8e2a10625eca872f1461cf140b90ece0
stijn.meul <stijn.meul@d79942cf-46e5-cbf0-422a-7ec671679747>
2010-12-03 16:29:22 +0000

    Modelklasses overgekopieerd en een paar dingen uitgecomment om te testen

--- trunk/peno3ws/src/be/kuleuven/cw/peno3/model/User.java
+++ trunk/peno3ws/src/be/kuleuven/cw/peno3/model/User.java
@@ -1,42 +1,78 @@
 package be.kuleuven.cw.peno3.model;
 
+import java.util.Date;
+
+//import android.location.Location;
+
+
 public class User {
-
-	protected String firstname;
-	protected String lastname;
-	protected Integer id;
-	protected Credential credentials;
+	private String userId;
+	private String firstName;
+	private String lastName;
+	private String password;
+	private int level;
+	private Date birthDate;
+	private ISP isp;
+//	private Location location;
 	
+	public User(String userId, String firstName, String lastName,String password, int level, Date birthDate, ISP isp) {
+		this.userId = userId;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		setPassword(password);
+		setLevel(level);
+		this.birthDate = birthDate;
+		setIsp(isp);
+	}
+	
+	private User() {}
+	
+//	public void setLocation(Location location)
+//	{
+//		this.location=location;
+//	}
+//	public Location getLocation()
+//	{
+//		return location;
+//	}
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public int getLevel() {
+		return level;
+	}
+
+	public void setLevel(int level) {
+		this.level = level;
+	}
+
+	public ISP getIsp() {
+		return isp;
+	}
+
+	public void setIsp(ISP isp) {
+		this.isp = isp;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
 	public String getFirstName() {
-		return firstname;
-	}
-	public void setFirstName(String firstName) {
-		this.firstname = firstName;
-	}
-	public String getLastName() {
-		return lastname;
-	}
-	public void setLastName(String lastName) {
-		this.lastname = lastName;
-	}
-	public Integer getId() {
-		return id;
-	}
-	public void setId(Integer id) {
-		this.id = id;
-	}
-	public Credential getCredential() {
-		return credentials;
-	}
-	public void setCredential(Credential credential) {
-		this.credentials = credential;
-	}
-	
-	@Override
-	public String toString() {
-		// TODO Auto-generated method stub
-		return "id : " + getId() + "\n" + "firstname : " + getFirstName() + "\n" + "lastname : " + getLastName() + "\nCredentials : \n" + getCredential();
+		return firstName;
 	}
 
+	public String getLastName() {
+		return lastName;
+	}
+
+	public Date getBirthDate() {
+		return birthDate;
+	}
 	
 }
