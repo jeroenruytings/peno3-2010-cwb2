cb828465098879e41de3fea312f68374aaabb9d3
fredy2479@hotmail.com <fredy2479@hotmail.com@d79942cf-46e5-cbf0-422a-7ec671679747>
2010-12-03 14:28:30 +0000

    met werkende URL gallery

--- Mobile Toledo/src/cw/kuleuven/be/peno1011/cwb2/controller/NavigationController.java
+++ Mobile Toledo/src/cw/kuleuven/be/peno1011/cwb2/controller/NavigationController.java
@@ -1,16 +1,25 @@
 
 package cw.kuleuven.be.peno1011.cwb2.controller;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.lang.reflect.Array;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 import java.util.ArrayList;
 import java.util.Iterator;
 
+import cw.kuleuven.be.peno1011.cwb2.R;
 import cw.kuleuven.be.peno1011.cwb2.database.BuildingDAO;
 import cw.kuleuven.be.peno1011.cwb2.model.Building;
 import cw.kuleuven.be.peno1011.cwb2.view.OwnLocationManager;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 
 import android.location.Criteria;
 import android.location.Location;
@@ -58,7 +67,7 @@ public class NavigationController{
 					//}
 			//}
 			
-			"haai","hoi","hoor","heftig","helding shana"
+			"haai","hoi","hoor","heftig","helding shana","Lise is de max!"
 		};
 		
 		return buildingnames;
@@ -83,11 +92,49 @@ public class NavigationController{
 		return existing;
 	}
 	
-	public Uri[] getPictureArray(String locationname)
+	  public Bitmap downloadFile(String fileUrl){
+		  Bitmap bmImg = null;
+          URL myFileUrl = null;          
+          try {
+               myFileUrl= new URL(fileUrl);
+          } catch (MalformedURLException e) {
+               // TODO Auto-generated catch block
+               e.printStackTrace();
+          }
+          try {
+               HttpURLConnection conn= (HttpURLConnection)myFileUrl.openConnection();
+               conn.setDoInput(true);
+               conn.connect();
+               int length = conn.getContentLength();
+               InputStream is = conn.getInputStream();
+               
+               bmImg = BitmapFactory.decodeStream(is);
+                              
+          } catch (IOException e) {
+               // TODO Auto-generated catch block
+               e.printStackTrace();
+          }
+          return bmImg;
+	  }
+
+	
+	public Bitmap[] getPictureArray(String locationname)
 	{
-		Uri[] pictureArray = {
-				Uri.parse("http://t2.gstatic.com/images?q=tbn:a-HKIJ5FgdO_VM:http://www.facebook.com/profile/pic.php?uid=AAAAAQAQZZMBjRztdGKy7zNLtrdyDAAAAApfkafD0SgsVaSJ4V-Hc7KA")
-		};
+		Bitmap img = downloadFile("http://www.trouwshop.nl/images/upload/20080907_234513_bruidspaar_gira.jpg");
+		Bitmap img2 = downloadFile("http://t2.gstatic.com/images?q=tbn:a-HKIJ5FgdO_VM:http://www.facebook.com/profile/pic.php?uid=AAAAAQAQZZMBjRztdGKy7zNLtrdyDAAAAApfkafD0SgsVaSJ4V-Hc7KA");
+		Bitmap img3 = downloadFile("http://www.appelogen.be/wp-images/derde_prentje.jpg");
+		Bitmap img4 = downloadFile("http://www.jouwpagina.nl/fotos2/k-mmygirl/catduck.jpg");
+		//String urlString = "http://www.trouwshop.nl/images/upload/20080907_234513_bruidspaar_gira.jpg";
+		//Uri[] pictureArray ={
+  
+				//Uri.parse("http://t2.gstatic.com/images?q=tbn:a-HKIJ5FgdO_VM:http://www.facebook.com/profile/pic.php?uid=AAAAAQAQZZMBjRztdGKy7zNLtrdyDAAAAApfkafD0SgsVaSJ4V-Hc7KA"),
+				//Uri.parse("http://www.appelogen.be/wp-images/derde_prentje.jpg"),
+				//Uri.parse("http://www.jouwpagina.nl/fotos2/k-mmygirl/catduck.jpg"),
+	//	Uri.parse(urlString)
+		//};
+		
+		Bitmap [] pictureArray = {img,img2,img3,img4};
+			
 		return pictureArray;
 	}
 	
