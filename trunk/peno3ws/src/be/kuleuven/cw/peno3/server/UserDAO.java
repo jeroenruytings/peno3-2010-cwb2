8a105c15282b93c474c25df13acdba0ab16b04f7
stijn.meul <stijn.meul@d79942cf-46e5-cbf0-422a-7ec671679747>
2010-12-03 17:03:36 +0000

    Superklasse DAO aangemaakt

--- trunk/peno3ws/src/be/kuleuven/cw/peno3/server/UserDAO.java
+++ trunk/peno3ws/src/be/kuleuven/cw/peno3/server/UserDAO.java
@@ -21,10 +21,7 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
 @Path ("/UserHandler")
-public class UserDAO {
-
-	protected DatabaseManager manager = DatabaseManager.getInstance();
-	private Cryptography cryptography = Cryptography.getInstance();
+public class UserDAO extends DAO{
 
 	@POST
 	@Path ("/getUserByName")
@@ -32,9 +29,7 @@ public class UserDAO {
 	public String getUserByName(@FormParam("name") String name){
 		String query = "SELECT * FROM user";
 		if(name !=null)query += " WHERE firstName like '%" + name + "%' or lastName like '%" + name + "%'";
-		String result = queryForUsers(query);
-		manager.disconnect();
-		return cryptography.encrypt(result);
+		return super.get(query);
 	}
 
 	@POST
@@ -43,9 +38,7 @@ public class UserDAO {
 	public String getUserByUserId(@FormParam("userId") String userId){
 		String query = "SELECT * FROM user";
 		if(userId !=null)query += " WHERE userId like '%" + userId + "%'";
-		String result = queryForUsers(query);
-		manager.disconnect();
-		return result;
+		return super.get(query);
 	}
 	
 	@POST
@@ -53,42 +46,7 @@ public class UserDAO {
 	@Produces ("application/json")
 	public String listUsers(){
 		String query = "SELECT * FROM user";
-		String result = queryForUsers(query);
-		manager.disconnect();
-		return result;
-	}
-
-	private String queryForUsers(String query) {
-		JsonArray users = new JsonArray();
-		ResultSet rs = manager.query(query);
-		Gson gson = new Gson();
-		try {
-			while(rs.next()) {
-				JsonObject user = (JsonObject) gson.toJsonTree(manager.getColumnValues(rs));
-				users.add(user);
-			}
-		} catch (SQLException e) {
-			JSONObject result = new JSONObject();
-			result.put("result", "SQLException : (ERR:" + e.getErrorCode() + ") " + e.getMessage());
-			return result.toString();
-		}
-		String asString = users.toString();
-		return asString;
-	}
-
-	private JsonArray querySimpleTable(String query) {
-		Vector users = new Vector();
-		ResultSet rs = manager.query(query);
-		Gson gson = new Gson();
-		try {
-			while(rs.next()) {
-				users.add(manager.getColumnValues(rs));
-			}
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return (JsonArray) gson.toJsonTree(users);
+		return super.list(query);
 	}
 
 	/*
@@ -98,19 +56,7 @@ public class UserDAO {
 	@Path ("/addUser")
 	@Produces ("application/json")
 	public String addUser(@FormParam("userId") String userId, @FormParam("firstName") String firstName, @FormParam("lastName") String lastName, @FormParam("password") String password, @FormParam("birthDate") String birthDate, @FormParam("rank") int rank){
-
-		JSONObject result = new JSONObject();
-		try {
-			String query = "INSERT INTO user (userId, firstName, lastName, password, birthDate, rank) VALUES ('"+ userId + "','" + firstName + "','" + lastName + "','" + password + "'," + birthDate + ",'" + rank +"')";
-			System.out.println(query);
-			manager.update(query);
-			manager.disconnect();
-			}
-		catch (SQLException e) {
-			result.put("result", "SQLException : (ERR:" + e.getErrorCode() + ") " + e.getMessage());
-			return result.toString();
-		}
-		result.put("result", "User sucessfully added");
-		return result.toString();
+		String query = "INSERT INTO user (userId, firstName, lastName, password, birthDate, rank) VALUES ('"+ userId + "','" + firstName + "','" + lastName + "','" + password + "'," + birthDate + ",'" + rank +"')";
+		return super.add(query);
 	}
 }
