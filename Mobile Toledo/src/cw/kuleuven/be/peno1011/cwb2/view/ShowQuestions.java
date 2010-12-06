7e44572efcae6e502e96eb5cdec6e624df9f9805
Jochentombal@gmail.com <Jochentombal@gmail.com@d79942cf-46e5-cbf0-422a-7ec671679747>
2010-12-06 23:08:37 +0000

    Er is een rating toegevoegd die je vanuit showQuestions kunt aanpassen

--- Mobile Toledo/src/cw/kuleuven/be/peno1011/cwb2/view/ShowQuestions.java
+++ Mobile Toledo/src/cw/kuleuven/be/peno1011/cwb2/view/ShowQuestions.java
@@ -1,19 +1,21 @@
 package cw.kuleuven.be.peno1011.cwb2.view;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import android.app.AlertDialog;
 import android.app.ListActivity;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.AdapterView.OnItemClickListener;
-import cw.kuleuven.be.peno1011.cwb2.model.Announcement;
-import cw.kuleuven.be.peno1011.cwb2.model.Course;
+import android.widget.RatingBar;
+import android.widget.RatingBar.OnRatingBarChangeListener;
+import android.widget.Toast;
+import cw.kuleuven.be.peno1011.cwb2.R;
 import cw.kuleuven.be.peno1011.cwb2.model.Lecture;
 import cw.kuleuven.be.peno1011.cwb2.model.Question;
 
@@ -51,24 +53,35 @@ public class ShowQuestions extends ListActivity {
 				
 				}
 		  });
+		 
+		  final RatingBar ratingbar = (RatingBar) findViewById(R.id.ratingbar);
+  		ratingbar.setOnRatingBarChangeListener(new OnRatingBarChangeListener() {
+  		    public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+  		        Toast.makeText(ShowQuestions.this, "New Rating: " + rating, Toast.LENGTH_SHORT).show();
+  		    }
+  		});      
 	  }
 		  				
 				
 	private void getInfoQuestion(Question question){
+		LayoutInflater inflater=LayoutInflater.from(ShowQuestions.this);
+		View questionview=inflater.inflate(R.layout.questionview, null);
+		
 		AlertDialog.Builder ab=new AlertDialog.Builder(ShowQuestions.this);
 		ab.setTitle(question.getQuestioner().getFirstName() + " "+ question.getQuestioner().getLastName());
 		ab.setMessage(question.getMessage());
+		ab.setView(questionview);
 		ab.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
 	
 	    	public void onClick(DialogInterface dialog, int whichButton) {
 			            	
-			            	
+	    		
 			        }
 			        });
 			        ab.show();	 
+	
 	}
 	
-		  
 }
 
 	
