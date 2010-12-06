a5f54b490c0ddd497e6a5983ab5bc9dde5071ec3
jeroenruytings <jeroenruytings@d79942cf-46e5-cbf0-422a-7ec671679747>
2010-12-06 18:34:12 +0000

    login bijna terug werkende

--- trunk/Mobile Toledo/src/cw/kuleuven/be/peno1011/cwb2/database/UserDAO.java
+++ trunk/Mobile Toledo/src/cw/kuleuven/be/peno1011/cwb2/database/UserDAO.java
@@ -6,6 +6,8 @@ import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpException;
 import org.apache.commons.httpclient.methods.PostMethod;
 
+import com.google.gson.Gson;
+
 import cw.kuleuven.be.peno1011.cwb2.model.User;
 
 
@@ -24,33 +26,37 @@ public class UserDAO {
 	public User getUser(String username) throws HttpException, IOException{
 		
 		HttpClient client = new HttpClient();
-		PostMethod method = new PostMethod("http://ariadne.cs.kuleuven.be/peno-cwb2/UserHandler/getUserByUserId");
+		PostMethod method = new PostMethod("http://ariadne.cs.kuleuven.be/peno-cwb2/UserHandler/getUserById");
 		method.addParameter("userId", username);
 		
 		int response = client.executeMethod(method);
-		String json = method.getResponseBodyAsString();
-		//String json = cryptography.decrypt(encryptedJson);
+		String encryptedJson = method.getResponseBodyAsString();
+		String json = cryptography.decrypt(encryptedJson);
 		
 		if(json.contains("[]")) {
 			return new User(null,null,null,null,-1,null,null);
 		}
 		
-		int start, end;
+		User user = new Gson().fromJson(json.toString(), User.class);  
 		
-		start = json.indexOf("userId")+9;
-		end = json.indexOf(",", start)-1;
-		String userId = json.substring(start,end);
-		start = json.indexOf("lastName")+11;
-		end = json.indexOf(",", start)-1;
-		String lastName = json.substring(start, end);
-		start = json.indexOf("firstName")+12;
-		end = json.indexOf(",", start)-1;
-		String firstName = json.substring(start, end);
-		start = json.indexOf("password")+11;
-		end = json.indexOf(",", start)-1;
-		String password = json.substring(start, end);
+//		int start, end;
 		
-		User user = new User(userId,firstName,lastName,password,1,null,null);
+//		start = json.indexOf("userId")+9;
+//		end = json.indexOf(",", start)-1;
+//		String userId = json.substring(start,end);
+//		start = json.indexOf("lastName")+11;
+//		end = json.indexOf(",", start)-1;
+//		String lastName = json.substring(start, end);
+//		start = json.indexOf("firstName")+12;
+//		end = json.indexOf(",", start)-1;
+//		String firstName = json.substring(start, end);
+//		start = json.indexOf("password")+11;
+//		end = json.indexOf(",", start)-1;
+//		String password = json.substring(start, end);
+//		
+//		User user = new User(userId,firstName,lastName,password,1,null,null);
+		
+		
 		return user;
 		
 	}
