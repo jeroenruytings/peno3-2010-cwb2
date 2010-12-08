882bba4b6dc3ee92db16dbf083a613f0532879a5
Jochentombal@gmail.com <Jochentombal@gmail.com@d79942cf-46e5-cbf0-422a-7ec671679747>
2010-12-08 12:46:26 +0000

    commentaar

--- Mobile Toledo/src/cw/kuleuven/be/peno1011/cwb2/view/ShowStatistics.java
+++ Mobile Toledo/src/cw/kuleuven/be/peno1011/cwb2/view/ShowStatistics.java
@@ -75,7 +75,9 @@ public class ShowStatistics extends Activity {
 			  //toont het aantal antwoorden in een progressbar
 			  //toont verschillende antwoorden in textview
 			  //toont deze enkel als ze bestaan, anders zijn ze leeg en dus niet gegeven.			  
-			 if (size == 2){
+			 
+			  //2vragen
+			  if (size == 2){
 								 
 	         final ProgressBar stata = (ProgressBar) findViewById(R.id.sa);
 	         stata.setProgress((a/answers.length)*100);
@@ -87,19 +89,22 @@ public class ShowStatistics extends Activity {
 	         statb.setProgress((b/answers.length)*100);
 	         TextView msgb = (TextView) findViewById(R.id.msgb);
 			 msgb.setText(answers[1]);
-	         
+			 
+	         //3vragen
 			 } else if (size == 3) {
 	         final ProgressBar statc = (ProgressBar) findViewById(R.id.sc);
 	         statc.setProgress((c/answers.length)*100);
 	         TextView msgc = (TextView) findViewById(R.id.msgc);
 			 msgc.setText(answers[2]);
-	         
+			 
+	         //4vragen
 			 } else if (size == 4) {
 	         final ProgressBar statd = (ProgressBar) findViewById(R.id.sd);
 	         statd.setProgress((d/answers.length)*100);
 	         TextView msgd = (TextView) findViewById(R.id.msgd);
 			 msgd.setText(answers[3]);
-	         
+			 
+	         //5vragen
 			 } else if (size == 5) {
 	         final ProgressBar state = (ProgressBar) findViewById(R.id.se);
 	         state.setProgress((e/answers.length)*100);
