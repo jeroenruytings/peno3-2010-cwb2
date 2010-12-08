e58d3fca4157dc22e1cd2c441b4e7114762f3883
Jochentombal@gmail.com <Jochentombal@gmail.com@d79942cf-46e5-cbf0-422a-7ec671679747>
2010-12-08 14:28:32 +0000

    Try en catch

--- Mobile Toledo/src/cw/kuleuven/be/peno1011/cwb2/view/CourseInfo.java
+++ Mobile Toledo/src/cw/kuleuven/be/peno1011/cwb2/view/CourseInfo.java
@@ -5,6 +5,7 @@ import java.util.List;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 import cw.kuleuven.be.peno1011.cwb2.R;
 import cw.kuleuven.be.peno1011.cwb2.controller.MainController;
 import cw.kuleuven.be.peno1011.cwb2.model.Course;
@@ -15,39 +16,45 @@ public class CourseInfo extends Activity{
 	     super.onCreate(savedInstanceState);
 	     setContentView(R.layout.courseinfo);
 	     
-	     Bundle bundle = getIntent().getExtras();
-		 final String courseTitle = (String) bundle.get("courseTitle");
-		 
-
-
-		 List<Course> courses = MainController.getUser().getIsp().getCourses();
-			String[] courseTitles = new String[courses.size()];
-			
-			Course course = null;
-			
-			for(int i = 0;i< courses.size();i++){
-				String courseName = courses.get(i).getCourseName();
-				courseTitles[i] = courseName;
-				if (courseTitle == courseName){
-					course = courses.get(i);
-				}
-			}
-		 
-				 
-		 String prof= course.getProf().getFirstName() + " " + course.getProf().getLastName();
-		 String code = course.getCourseCode();
-		 String year = course.getAcademicYear();
-		 
-		 TextView proftxt = (TextView) findViewById(R.id.prof);
-		 proftxt.setText("professor" + ": " + prof);
-		 
-		 TextView codetxt = (TextView) findViewById(R.id.code);
-		 codetxt.setText("code van de cursus" + ": " + code);
-		 
-		 TextView yeartxt = (TextView) findViewById(R.id.year);
-		 yeartxt.setText("academiejaar" + ": " + year);
-		 
+	     try{
 	     
-
+		     Bundle bundle = getIntent().getExtras();
+			 final String courseTitle = (String) bundle.get("courseTitle");
+			 
+	
+	
+			 List<Course> courses = MainController.getUser().getIsp().getCourses();
+				String[] courseTitles = new String[courses.size()];
+				
+				Course course = null;
+				
+				for(int i = 0;i< courses.size();i++){
+					String courseName = courses.get(i).getCourseName();
+					courseTitles[i] = courseName;
+					if (courseTitle == courseName){
+						course = courses.get(i);
+					}
+				}
+			 
+					 
+			 String prof= course.getProf().getFirstName() + " " + course.getProf().getLastName();
+			 String code = course.getCourseCode();
+			 String year = course.getAcademicYear();
+			 
+			 TextView proftxt = (TextView) findViewById(R.id.prof);
+			 proftxt.setText("professor" + ": " + prof);
+			 
+			 TextView codetxt = (TextView) findViewById(R.id.code);
+			 codetxt.setText("code van de cursus" + ": " + code);
+			 
+			 TextView yeartxt = (TextView) findViewById(R.id.year);
+			 yeartxt.setText("academiejaar" + ": " + year);
+		
+		}
+	     catch(NullPointerException ne){
+	    	 Toast.makeText(getApplicationContext(), "Fout bij het opvragen van het vak, probeer nog eens of log nog eens opnieuw in.",
+			          Toast.LENGTH_LONG).show();
+	     }
+	     
 	}
 }
