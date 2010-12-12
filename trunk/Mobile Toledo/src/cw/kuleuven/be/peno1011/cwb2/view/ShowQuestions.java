47af7aa2adc655373b7e5eb20af9abe4d65e083e
jeroenruytings <jeroenruytings@d79942cf-46e5-cbf0-422a-7ec671679747>
2010-12-12 21:57:27 +0000

    Voorlopige update questions

--- trunk/Mobile Toledo/src/cw/kuleuven/be/peno1011/cwb2/view/ShowQuestions.java
+++ trunk/Mobile Toledo/src/cw/kuleuven/be/peno1011/cwb2/view/ShowQuestions.java
@@ -26,47 +26,50 @@ public class ShowQuestions extends ListActivity {
 	
 	public void onCreate(Bundle savedInstanceState) {
 	   super.onCreate(savedInstanceState);
-		  
-		try{
-		  //haalt lecture
-		  Bundle bundle = getIntent().getExtras();
-		  final Lecture lecture = (Lecture) bundle.get("lecture");		  
-		  final ArrayList<Question> questions = lecture.getQuestions();
-		  
-		  //Plaatst een titel vanboven
-		  requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);            
-          setContentView(R.layout.showquestions);
-          getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.title);         
-          ((TextView)findViewById(R.id.titlebar)).setText("Vragen");
-          
-		  // maakt een string[] displayStrings aan van de alles messages van de questions
-          // De users zijn er niet bijgevoegd wegens wens van anonimiteit.
-          //TODO De database sorteren op score, dit is een integer van 0 tot 5
-		  final String[] displayStrings = new String[questions.size()];
-		  	  for(int i = 0;i< questions.size();i++){
-					
-		  		  	String displayString = "Vraag"+ ": " + questions.get(i).getMessage() + "(" + questions.get(i).getAppreciation() + " sterren)";
-					displayStrings[questions.size()-i-1] = displayString;
-					
-		  	  }
-		  
-		  // Maakt een listAdapter met als layout de showquestions.xml en als input de string[] displayStrings  
-		  setListAdapter(new ArrayAdapter<String>(this,R.layout.showquestions, displayStrings));
-		  ListView lv = getListView();
-		  lv.setTextFilterEnabled(true);
-		  lv.setOnItemClickListener(new OnItemClickListener() {
-				@Override
-				public void onItemClick(AdapterView<?> parent, View view,int position, long id) {						
-					getInfoQuestion(questions.get(questions.size()-position-1));	
+       
+	   setContentView(R.layout.showquestions);
+  
+	  //haalt lecture
+	  Bundle bundle = getIntent().getExtras();
+	  final Lecture lecture;
+	  
+	  if(bundle != null){
+			lecture = (Lecture) bundle.get("lecture");
+		}
+		else{
+			lecture = null;
+			//finish();
+		}	  
+	  final ArrayList<Question> questions = lecture.getQuestions();
+	  
+	  //Plaatst een titel vanboven
+	  requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);            
+      getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.title);         
+      ((TextView)findViewById(R.id.titlebar)).setText("Vragen");
+      
+	  // maakt een string[] displayStrings aan van de alles messages van de questions
+      // De users zijn er niet bijgevoegd wegens wens van anonimiteit.
+      //TODO De database sorteren op score, dit is een integer van 0 tot 5
+	  final String[] displayStrings = new String[questions.size()];
+	  	  for(int i = 0;i< questions.size();i++){
 				
-				}
-		  });
-		}
-		catch(NullPointerException ne){
-			Toast.makeText(getApplicationContext(), "Fout bij het opvragen van de vragen.",
-			          Toast.LENGTH_LONG).show();
-		}
-		      
+	  		  	String displayString = "Vraag"+ ": " + questions.get(i).getMessage() + "(" + questions.get(i).getAppreciation() + " sterren)";
+				displayStrings[questions.size()-i-1] = displayString;
+				
+	  	  }
+	  
+	  // Maakt een listAdapter met als layout de showquestions.xml en als input de string[] displayStrings  
+	  setListAdapter(new ArrayAdapter<String>(this,R.layout.showquestions, displayStrings));
+	  ListView lv = getListView();
+	  lv.setTextFilterEnabled(true);
+	  lv.setOnItemClickListener(new OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view,int position, long id) {						
+				getInfoQuestion(questions.get(questions.size()-position-1));	
+			
+			}
+	  });
+	      
 	  }
 		  				
 		//dit is de functie van als er op een willekeurige vraag wordt geklikt		
