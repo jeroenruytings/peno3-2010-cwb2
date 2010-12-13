f5c0dd9ecf344f77bb275ed7b3778f9cf107e1e4
jeroenruytings <jeroenruytings@d79942cf-46e5-cbf0-422a-7ec671679747>
2010-12-13 12:28:48 +0000

    Announcements tonen werkt weer :)

--- trunk/Mobile Toledo/src/cw/kuleuven/be/peno1011/cwb2/database/AnnouncementDAO.java
+++ trunk/Mobile Toledo/src/cw/kuleuven/be/peno1011/cwb2/database/AnnouncementDAO.java
@@ -113,36 +113,42 @@ public class AnnouncementDAO {
 
 	public List<Announcement> getRecentAnnouncements(Course course, int numberofdays){
 			HttpClient client = new HttpClient();
-	        PostMethod method = new PostMethod("http://ariadne.cs.kuleuven.be/peno-cwb2/AnnouncementHandler/getAnnouncementByStartDateAndCourse");
+	        PostMethod method = new PostMethod("http://ariadne.cs.kuleuven.be/peno-cwb2/AnnouncementHandler/getAnnouncementByStartDateAndCourseCode");
 	        
 	        Date date = new Date();
 	        date.setDate(date.getDate()-numberofdays);
 	        
 	        method.addParameter("courseCode", course.getCourseCode());
-	        //method.addParameter("date",cryptography.toMysqlDate(date));
+	        method.addParameter("date",cryptography.toMysqlDate(date));
 //	        Calendar calendar = Calendar.getInstance();
 //	        calendar.setTime(date);
 //	        String sqlDate = "" + calendar.YEAR + calendar.MONTH + calendar.DAY_OF_MONTH
 //	        	+ calendar.HOUR_OF_DAY + calendar.MINUTE + calendar.SECOND;
-	        String maand = null, dag = null, uren = null, minuten = null, seconden = null;
-	        int year = date.getYear()+1900;
-	        int month = date.getMonth()+1;
-	        if(month<10)
-	        	maand = "0"+month;
-	        int day = date.getDay();
-	        if(day<10)
-	        	dag = "0"+day;
-	        int hours = date.getHours();
-	        if(hours<10)
-	        	uren = "0"+hours;
-	        int minutes = date.getMinutes();
-	        if(minutes<10)
-	        	minuten = "0"+minutes;
-	        int seconds = date.getSeconds();
-	        if(seconds<10)
-	        	seconden = "0"+seconds;
-			String sqlDate = "" + year + maand + dag + uren + minuten + seconden; 
-	        method.addParameter("date", sqlDate);
+//	        
+//	        String maand = null, dag = null, uren = null, minuten = null, seconden = null;
+//	        int year = date.getYear()+1900;
+//	        int month = date.getMonth()+1;
+//	        maand = ""+month;
+//	        if(month<10)
+//	        	maand = "0"+month;
+//	        int day = date.getDate();
+//	        dag = ""+day;
+//	        if(day<10)
+//	        	dag = "0"+day;
+//	        int hours = date.getHours();
+//	        uren = ""+hours;
+//	        if(hours<10)
+//	        	uren = "0"+hours;
+//	        int minutes = date.getMinutes();
+//	        minuten = ""+minutes;
+//	        if(minutes<10)
+//	        	minuten = "0"+minutes;
+//	        int seconds = date.getSeconds();
+//	        seconden = ""+seconds;
+//	        if(seconds<10)
+//	        	seconden = "0"+seconds;
+//			String sqlDate = "" + year + maand + dag + uren + minuten + seconden; 
+//	        method.addParameter("date", sqlDate);
 	        
 	        String json = null;
 			try {
